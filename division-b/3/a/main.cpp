e279578cd443cf7fb99ffa4cca4bd7e2f18628cb
hushkeev <hushkeev@protei.ru>
2021-10-02 22:42:45 +0300

    Finish b-3-a

--- division-b/3/a/main.cpp
+++ division-b/3/a/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
+#include <unordered_set>
+#include <sstream>
+
+std::unordered_set<int> GetLineNumbers()
+{
+    std::unordered_set<int> result;
+    std::string line;
+    std::getline(std::cin, line);
+    std::stringstream splitter(line);
+    while (!splitter.eof())
+    {
+        int number;
+        splitter >> number;
+        result.insert(number);
+    }
+    return result;
+}
 
 int main()
 {
+    auto firstLineNumbers = GetLineNumbers();
+    auto secondLineNumbers = GetLineNumbers();
+    size_t interSectionCount = 0;
+    for (auto& secondLineNumber : secondLineNumbers)
+    {
+        if (firstLineNumbers.count(secondLineNumber) != 0)
+            ++interSectionCount;
+    }
+    std::cout << interSectionCount;
     return 0;
 }
