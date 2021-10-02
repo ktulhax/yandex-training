248a7c826bdb1ade7f0c0db5d196171c024231a6
hushkeev <hushkeev@protei.ru>
2021-10-03 01:50:37 +0300

    Finish b-4-с

--- division-b/4/c/main.cpp
+++ division-b/4/c/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <unordered_map>
+#include <set>
+#include <map>
 
 int main()
 {
+    std::string inWord;
+    std::unordered_map<std::string, unsigned> mapOfWords;
+    while (std::cin >> inWord)
+        mapOfWords[inWord]++;
+    std::map<unsigned, std::set<std::string>, std::greater<>> wordsSortedByCount;
+    for (auto& [word, wordCount] : mapOfWords)
+        wordsSortedByCount[wordCount].insert(word);
+    for (auto& [_, words] : wordsSortedByCount)
+    {
+        for (auto& word : words)
+            std::cout << word << std::endl;
+    }
     return 0;
 }
