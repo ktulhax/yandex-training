db9d16eeb1d05aebab6e788a6614369b597c7e31
hushkeev <hushkeev@protei.ru>
2021-10-03 01:19:31 +0300

    Finish b-4-a

--- division-b/4/a/main.cpp
+++ division-b/4/a/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <map>
+#include <cstdint>
 
 int main()
 {
+    int n;
+    std::cin >> n;
+    std::map<int64_t, int64_t> result;
+    for (int i = 0; i < n; ++i)
+    {
+        int64_t a, d;
+        std::cin >> d >> a;
+        result[d] += a;
+    }
+    for (auto& [d, a] : result)
+        std::cout << d << " " << a << std::endl;
     return 0;
 }
