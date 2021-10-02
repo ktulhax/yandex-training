c395bff6cef3a0a49e4d9227d21fc846137e24e2
hushkeev <hushkeev@protei.ru>
2021-10-02 23:10:17 +0300

    Finish b-3-c

--- division-b/3/c/main.cpp
+++ division-b/3/c/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <sstream>
+#include <unordered_set>
+#include <algorithm>
+#include <vector>
 
 int main()
 {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream iss(line);
+    std::vector<int> numbers;
+    std::unordered_set<int> moreThanOneTimeNumbers;
+    int inNumber = 0;
+    while (iss >> inNumber)
+    {
+        if (moreThanOneTimeNumbers.count(inNumber) != 0)
+            continue;
+        auto it = std::find(numbers.begin(), numbers.end(), inNumber);
+        if (it != numbers.end())
+        {
+            numbers.erase(it);
+            moreThanOneTimeNumbers.insert(inNumber);
+            continue;
+        }
+        numbers.push_back(inNumber);
+    }
+    for (auto& outNumber : numbers)
+        std::cout << outNumber << " ";
     return 0;
 }
