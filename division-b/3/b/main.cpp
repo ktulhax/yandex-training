1d5255ae64fa79f93f849e85dbbcc79560adc2de
hushkeev <hushkeev@protei.ru>
2021-10-02 22:49:02 +0300

    Finish b-3-b

--- division-b/3/b/main.cpp
+++ division-b/3/b/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <unordered_set>
 
 int main()
 {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream iss(line);
+    int number = 0;
+    std::unordered_set<int> prevNumbers;
+    while (iss >> number)
+    {
+        std::cout << (prevNumbers.count(number) != 0 ? "YES" : "NO") << std::endl;
+        prevNumbers.insert(number);
+    }
     return 0;
 }
