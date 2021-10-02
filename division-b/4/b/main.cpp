8b8cf097d5e310001d717d5b4ee854729827091e
hushkeev <hushkeev@protei.ru>
2021-10-03 01:27:03 +0300

    Finish b-4-b

--- division-b/4/b/main.cpp
+++ division-b/4/b/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <map>
 
 int main()
 {
+    std::map<std::string, unsigned> votes;
+    std::string participant;
+    unsigned countOfVotes = 0;
+    while (std::cin >> participant && std::cin >> countOfVotes)
+        votes[participant] += countOfVotes;
+    for (auto& [p, c] : votes)
+        std::cout << p << " " << c << std::endl;
     return 0;
 }
